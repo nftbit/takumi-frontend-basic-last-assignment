137b3a350f6304786dcb6ec10a7b5e1677c840d8
nftbit <t.kawamoto@4nonome.com>
2023-02-24 17:12:16 +0900

    Revert "molucules-message作成"
    This reverts commit b1d3ff54e6e155cdfeaae3787ce59369418b8ddd.

--- src/stories/molecules-message.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Message from "../components/molecules/massage/message.html";
-import "../components/molecules/massage/message.scss";
-
-export default { title: "Molecules/Message"};
-
-export const Message = () => Item_Message; 
