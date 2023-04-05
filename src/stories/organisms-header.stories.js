255e005e795223231f773239e0415b97282880af
nftbit <t.kawamoto@4nonome.com>
2023-04-06 01:59:11 +0900

    organisms-header-init

---
+++ src/stories/organisms-header.stories.js
@@ -0,0 +1,6 @@
+import Item_header from "../components/organisms/header/header.html";
+import "../components/organisms/header/header.scss";
+
+export default {title: "Organisms/Header"};
+
+export const Header = () => Item_header;
